0b948fdc1d8a59b871255ebbb42fd6153760f04c
DanyPlaya <danila.lusuy@gmail.com>
2023-02-26 15:31:14 +0300

    test

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
-import { borderRadius } from '@mui/system';
+
 const SearchBar = () => {
   return (
     <Paper
